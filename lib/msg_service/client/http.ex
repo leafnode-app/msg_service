0e3691ff2b4ce6591a0dd017796cb1c9d33e8f55
Torean Joel <toreanjoel@gmail.com>
2024-08-21 01:28:45 +0200

    feat: add tesla HTTP client

---
+++ lib/msg_service/client/http.ex
@@ -0,0 +1,11 @@
+defmodule MsgService.Client.Http do
+  @moduledoc """
+  HTTP client used to make requests to other services externally
+  """
+
+  # TODO: add a timeout
+  # TOOD: add base and request types
+  # TOOD: add the configs with the details to be used in the client module
+  # TOOD: create secrets file for the keys in for Twilio
+  # TOOD: Methods for, sending messages back to a specific client
+end
